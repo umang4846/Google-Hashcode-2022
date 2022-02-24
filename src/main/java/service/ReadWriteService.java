7b8f6d1489e20979a110d0d633c159e6705bea7d
Umang4846 <you@example.com>
2022-02-25 02:10:19 +0530

    updated

--- src/main/java/service/ReadWriteService.java
+++ src/main/java/service/ReadWriteService.java
@@ -1,10 +1,13 @@
 package service;
 
 import model.Problem;
+import model.Solution;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public interface ReadWriteService {
     Problem read(File file) throws FileNotFoundException;
+    void write(Solution solution, File fileOutputPath) throws IOException;
 }
